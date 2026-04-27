d929b502c044a047a5db6c5616d370ad648c54b1
Ali Hashemi <hashemi1997ali@gmail.com>
2026-04-27 13:35:14 +0200

    add multi-page build for journal

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        journal: resolve(__dirname, "journal.html"),
+      },
+    },
+  },
+});
